f1ffdb517dc75bcc673ed8c8064f24f73847f9d2
Wusiki Jeronii <wusikijeronii@gmail.com>
2024-10-25 12:44:35 +0300

    ServiceDispatch integrate

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,16 +8,16 @@ namespace tests
 
 int main(int argc, char *argv[])
 {
-    logging::LogManager::init();
-    auto logger = logging::mng->addLogger<logging::ConsoleLogger>("backend");
-    logging::mng->level(logging::Level::Trace);
-    logger->setPattern("%(color_auto)%(level_name)\t%(message)%(color_off)\n");
-    logging::mng->defaultLogger(logger);
+    task::ServiceDispatch sd;
+    logging::g_LogService = astl::alloc<logging::LogService>();
+    sd.registerService(logging::g_LogService);
+    logging::g_DefaultLogger = logging::g_LogService->addLogger<logging::ConsoleLogger>("app");
+    logging::g_LogService->level(logging::Level::Trace);
+    logging::g_DefaultLogger->setPattern("%(color_auto)%(level_name)\t%(message)%(color_off)\n");
     const char *dataDir = getenv("TEST_DATA_DIR");
     const char *outputDir = getenv("TEST_OUTPUT_DIR");
     if (!dataDir || !outputDir) return 1;
     bool result = tests::runTest(dataDir, outputDir);
-    logging::mng->await();
-    logging::LogManager::destroy();
+    logging::g_LogService->await();
     return result ? 0 : 1;
 }
